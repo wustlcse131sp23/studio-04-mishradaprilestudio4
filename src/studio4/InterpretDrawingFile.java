a83a64195707e107506dfeb2fa2f6dcf91c1d925
Judyli <Judyli@localhost>
2023-02-23 12:08:16 -0600

    !

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,39 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+			System.out.println(shapeType);
+			
+		int redComponent = in.nextInt();
+			System.out.println(redComponent);
+			
+		int greenComponent = in.nextInt();
+			System.out.println(greenComponent);
+			
+		int blueComponent = in.nextInt();
+			System.out.println(blueComponent);
+			
+			boolean booleanisFilled = in.nextBoolean();
+			System.out.println(booleanisFilled);
+			
+	double x = in.nextDouble();
+
+	double y = in.nextDouble();
+	double halfWidth = in.nextDouble();
+	double halfHeight = in.nextDouble();
+	
+	if (booleanisFilled == true) {
+StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+	StdDraw.filledRectangle(x,  y,  halfWidth, halfHeight);
+	
+	} else {
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+	StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			
 	}
-}
+			
+		}
+		
+	}
+
